eb8609863c816e7710fa5dc93a1a0ecfbe8cdb36
cibs84 <d.tilesi@gmail.com>
2022-07-09 23:09:24 +0200

    SpinnerLoader component completed

--- src/components/AlbumsList.vue
+++ src/components/AlbumsList.vue
@@ -1,7 +1,11 @@
 <template>
-    <section class="albums-section">
+    <section class="albums-section" :class="{loader: !this.transferCompleted}">
         <div class="container">
-            <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5">
+            <!-- Le cards album saranno visualizzate SE sarà completato il trasferimento dei dati relativi agli album -->
+            <div 
+                class="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5"
+                v-if="this.transferCompleted"
+            >
                 <AlbumCard 
                     v-for="(singleAlbum, index) in dataAlbums" 
                     :key="index" 
@@ -11,6 +15,9 @@
                     :year="singleAlbum.year"
                 />
             </div>
+
+            <!-- ALTRIMENTI sarà visualizzato lo SpinneLoader fino al completamento del trasferimento dati -->
+            <SpinnerLoader v-else/>
         </div>
     </section>
 </template>
@@ -18,25 +25,32 @@
 
 <script>
 import AlbumCard from "./AlbumCard.vue";
+import SpinnerLoader from "./SpinnerLoader.vue";
 import axios from "axios";
 
 export default {
     name: 'AlbumsList',
     components: {
-        AlbumCard
+        AlbumCard,
+        SpinnerLoader
     },
     data(){
         return {
             url: "https://flynn.boolean.careers/exercises/api/array/music",
             dataAlbums : [],
-            transferCompleted: false
+            transferCompleted: false,
         }
     },
     methods: {
+        // Preleva i dati degli album con una chiamata all'endpoint contenuto nella variabile 'url'
         getDataAlbums() {
             axios.get(this.url).then(response => {
+                // Ciò che restituisce il server chiamato, sarà memorizzato nell'array 'dataAlbums' precedentemente creato
                 this.dataAlbums = response.data.response;
-                this.transferCompleted = true;
+                // Per testare lo spinner loader è stato impostato un delay per rendere true la variabile di controllo del trasferimento completato
+                setTimeout(() => {
+                    this.transferCompleted = true;
+                }, 2000);
             })
             .catch(err => {
                 console.log("Error: ", err)
@@ -44,9 +58,10 @@ export default {
         }
     },
     created() {
+        // Prima della creazione del DOM invoco la funzione per prelevare i dati relativi agli album
         this.getDataAlbums();
     }
-}
+}   
 </script>
 
 
@@ -60,5 +75,11 @@ export default {
     .container {
         max-width: 1100px;
     }
+}
+.albums-section.loader {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
 }
 </style>
---
+++ src/components/SpinnerLoader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="text-center">
+        <div class="spinner-border text-light" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'SpinnerLoader'
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
